8600bb31f7137e968d51d5e5dfec8c85dcb7945a
sabir <sksabiruddin81@gmail.com>
2024-01-11 15:55:34 +0530

    first commit

---
+++ url.js
@@ -0,0 +1,4 @@
+// export const URL = "http://localhost:5000";
+// export const IF = "http://localhost:5000/images/";
+export const URL = "https://my-blog-ardp.onrender.com";
+export const IF = "https://my-blog-ardp.onrender.com/images/";
